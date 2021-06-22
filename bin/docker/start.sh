c090946e121f10bb86cab2f8882a9f5979553d08
John Beynon <jbeynon@salesforce.com>
2021-06-22 09:44:06 +0100

    Make hostname configurable

--- bin/docker/start.sh
+++ bin/docker/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 /usr/src/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-until /usr/src/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=render-app
+until /usr/src/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME}
 do
     sleep 0.1
 done
